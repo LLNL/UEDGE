4e371dcf5ec26a71c01427f3a8e2296256e59d7f
Jerome Guterl <guterlj@fusion.gat.com>
2020-10-04 09:00:04 -0700

    remove UEDGEToolBox from uedge package  __init__.py. UEDGEToolBox is now a separate package which works with any version of uedge (python)

--- pyscripts/__init__.py
+++ pyscripts/__init__.py
@@ -9,10 +9,4 @@ except:
     except:
         __version__ = 'unknown'
 print('# UEDGE version:',__version__)        
-from .UEDGESettings import *
-from .UEDGESimulation import *    
-from .UEDGEDoc import *
-from .UEDGEToolBox import *       
-from .UEDGEBas2Py import *
-#__all__=["UEDGEToolBox","UEDGESettings"]
 
