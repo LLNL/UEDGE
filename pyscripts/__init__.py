3219b1d8f2c3aec17e6a99f02cb9e9b65b009976
Jerome Guterl <guterlj@fusion.gat.com>
2020-10-05 22:16:37 -0700

    get githash and add it to __version__.py

--- pyscripts/__init__.py
+++ pyscripts/__init__.py
@@ -3,10 +3,13 @@ from . import *
 
 try:
     from uedge.__version__ import __version__
+    from uedge.__version__ import GitHash
 except:
     try:
         from __version__ import __version__
+        from __version__ import GitHash
     except:
         __version__ = 'unknown'
-print('# UEDGE version:',__version__)        
-
+        GitHash=None
+print('>>> UEDGE version:',__version__) 
+print('>>> UEDGE Git SHA:,',GitHash)    
