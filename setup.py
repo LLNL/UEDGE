e2fc00ebafb21ff40e676b4e1e5931b2cbf15a9a
Jerome Guterl <guterlj@fusion.gat.com>
2020-10-14 11:44:16 -0700

    add cargs in setup.py

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ else:
     
 FLAGS ='DEBUG = -v --fargs "{}"'.format(' '.join(FARGS))
 if CARGS!=[]:
-    FLAGS =FLAGS+' --cargs={}'.format(' '.join(CARGS))
+    FLAGS =FLAGS+' --cargs="{}"'.format(' '.join(CARGS))
 
 
 if petsc == 1 and os.getenv('PETSC_DIR') == None:
@@ -250,7 +250,7 @@ setup(name="uedge",
                              libraries=libraries,
                              define_macros=define_macros,
                              extra_objects=uedgeobjects,
-                             extra_link_args=['-fopenmp']+['-g','-DFORTHON'] +
+                             extra_link_args=CARGS+['-g','-DFORTHON'] +
                              fcompiler.extra_link_args,
                              extra_compile_args=fcompiler.extra_compile_args
                              )],
