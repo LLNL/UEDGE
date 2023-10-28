1d2844f27be6d4537ad2e65294c8367ae9c7dd83
Andreas Holm <60451789+holm10@users.noreply.github.com>
2023-10-28 16:43:21 -0700

    Removes odesetup clutter

--- bbb/odesetup.m
+++ bbb/odesetup.m
@@ -6525,8 +6525,6 @@ c_mpi         call MPI_BARRIER(uedgeComm, ierr)
            icall = 1
          endif
 
-
-
 c   Check model switches for UEDGE updates/bugs
       if (oldseec .gt. 0) then
             write(*,*) "        **** WARNING ****"
@@ -6555,6 +6553,10 @@ c   Check model switches for UEDGE updates/bugs
                 call xerrab("Error: jhswitch>0 used without override=1")
             endif
       endif
+
+
+
+
 c_mpi         call MPI_BARRIER(uedgeComm, myfoo)
 
          imeth = inewton(igrid)
