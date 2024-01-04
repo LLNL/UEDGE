05df0155e093681ae5058c9ab5cd658b4bdd72b2
Andreas Holm <60451789+holm10@users.noreply.github.com>
2024-01-03 17:23:26 -0800

    Adds citation instructions
    And a veery snazzy UEDGE logo

--- pyscripts/__init__.py
+++ pyscripts/__init__.py
@@ -1,6 +1,23 @@
 from .uedge import *
 from os import path
 from pathlib import Path
+
+print('''             __  ____________  ____________
+            / / / / ____/ __ \/ ____/ ____/
+           / / / / __/ / / / / / __/ __/   
+          / /_/ / /___/ /_/ / /_/ / /___   
+          \____/_____/_____/\____/_____/
+''')
+
+
+print('''
+======================= NOTE =======================
+If you publish or present UEDGE results, you are
+required to properly cite the use of UEDGE according
+to the instructions at:
+        http://software.llnl.gov/UEDGE/citing
+''')
+
 try:
     from uedge.__version__ import __version__
     from uedge.__src__ import __src__
@@ -13,6 +30,7 @@ except:
     except:
         __version__ = 'unknown'
         __src__ = 'unknown'
+
 
 #
 # Load the startup file .uedgerc.py from cwd or home.
@@ -34,3 +52,8 @@ elif path.exists(_homefile):
 #
 
 uedge.bbb.uedge_ver = __version__
+
+#
+# Display text instructing on use of citations
+#
+
