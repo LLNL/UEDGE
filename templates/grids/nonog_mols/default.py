28b45e935006394afaeef538bbfb9b5dff29e2dc
Andreas Holm <60451789+holm10@users.noreply.github.com>
2024-01-10 15:17:11 -0800

    Adds grid and solution for molecules

---
+++ templates/grids/nonog_mols/default.py
@@ -0,0 +1,10 @@
+bbb.mhdgeo = 1 		#use MHD equilibrium
+bbb.gengrid=0		#read mesh from gridue file
+com.geometry = "snull"
+com.isnonog = 1		#nonorthogal differencing used
+
+bbb.methn = 33		#ion continuty eqn
+bbb.methu = 33		#ion parallel momentum eqn
+bbb.methe = 33		#electron energy eqn
+bbb.methi = 33		#ion energy eqn
+bbb.methg = 66		#neutral gas continuity eqn
